4112f501f66088fb28ecc19eae0e24a9047fb975
mimiminimalizm <maria.melnik.a@gmail.com>
2017-04-23 16:57:31 +0300

    Ready simple SMTP-Client without exceptions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,27 +27,63 @@ namespace SMTP_Client
             InitializeComponent();
         }
 
-        private bool isCorrect(String from, String to, String body, String password) {
+        private bool isCorrectMail(String to, String body) {
             bool result = false;
-
+            if (to != "" && body != "") {
+                result = true;
+            }
             return result;
-        } 
+        }
+
+        private bool isCorrectLogin(String mail, String password) {
+            if (mail == "" || password == "") {
+                return false;
+            }
+            return true;
+        }
 
         private void btnSendMail_Click(object sender, RoutedEventArgs e)
         {
-            
-            MailAddress from = new MailAddress("maria.melnik.a@gmail.com", text_Name.Text);
-            MailAddress to = new MailAddress(text_To.Text);
+            if (!isCorrectMail(text_To.Text, text_Data.Text)) {
+                System.Windows.MessageBox.Show("Отсутствует содержание либо адрес получателя!", "Ошибка", MessageBoxButton.OK);
+                return;
+            }
+            if (!isCorrectLogin(login.Text, password.Password))
+            {
+                System.Windows.MessageBox.Show("Отсутствуют аутентификационные данные!", "Ошибка", MessageBoxButton.OK);
+                return;
+            }
+
+            MailAddress from = new MailAddress(login.Text, text_Name.Text);
+            MailAddress to;
+            try
+            {
+                 to = new MailAddress(text_To.Text);
+
+            }
+            catch (FormatException) {
+                System.Windows.MessageBox.Show("Неверный адрес получателя!", "Ошибка", MessageBoxButton.OK);
+                return;
+            }
 
             MailMessage m = new MailMessage(from, to);
             m.Subject = text_Subject.Text;
             m.Body = text_Data.Text;
             m.IsBodyHtml = false;
             SmtpClient smtp = new SmtpClient("smtp.gmail.com", 25);
-            smtp.Credentials = new NetworkCredential("maria.melnik.a@gmail.com", "vfhbzvtkmybr1932289");
+            smtp.Credentials = new NetworkCredential(login.Text, password.Password); 
             smtp.EnableSsl = true;
-            smtp.Send(m);
+            try
+            {
+                smtp.Send(m);
+            }
+            catch (Exception) {
+                System.Windows.MessageBox.Show("Были введены неверные аутентификационные данные либо серверу не удалось установить соединение!", "Ошибка", MessageBoxButton.OK);
+                return;
+            }
             System.Windows.MessageBox.Show("Сообщение было отправлено!", "Операция прошла успешно", MessageBoxButton.OK);
+            text_Data.Text = "";
+            text_Subject.Text = "";
         }
     }
 }
